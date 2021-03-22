5fcbdaef8273ae92f18f0d629fc4c7603f5ff031
rahirim93 <59837269+rahirim93@users.noreply.github.com>
2021-03-22 19:08:36 +0300

    Последняя версия

--- bluetooth_teapot/bluetooth_teapot.ino
+++ bluetooth_teapot/bluetooth_teapot.ino
@@ -1,40 +1,21 @@
-long delayPeriod = 35000;
+long delayPeriod = 14000;
 long counterPeriod = 0;
-long counterFlash = 0;
 boolean flagOn;
 long counterPeriodTemp = 0;
 
-boolean teapotIsOn;
-
 char val;
-String val2 = "";
-long periodWake = 5000;
-long counter = 0;
 
-float tempC;
-int reading;
+float tempC;//Переменная для хранения температуры в 
+int reading;//Перменная для хранения аналогового значения температуры
 void setup() {
   analogReference(INTERNAL);// включаем внутрений источник опорного 1,1 вольт
 
   pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
 
   digitalWrite(2, 0);
-  digitalWrite(3, 0);
-  digitalWrite(4, 0);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
-
-
+ 
   Serial.begin(9600);
-  // put your setup code here, to run once:
+  
   digitalWrite(13, 1);
 }
 
@@ -47,15 +28,14 @@ void loop() {
 }
 
 void funChar() {
-
   if (Serial.available()) {
     val = Serial.read();
     //Serial.println(val);
-    // При символе "1" включаем светодиод
+    // При символе "1" выключаем чайник
     if (val == '1') {
       digitalWrite(13, 1);
     }
-    // При символе "0" выключаем светодиод
+    // При символе "0" включаем чайник
     if ( val == '0') {
       digitalWrite(13, 0);
     }
@@ -63,6 +43,7 @@ void funChar() {
 }
 //Функция для пробуждения посредтвом включения светодидов через определенные промежутки времени
 void flash() {
+  //Если прошло времени больше чем время задежрки, включить нагрузки пробуждения
   if (millis() - counterPeriod > delayPeriod) {
     counterPeriod = millis();
     digitalWrite(2, 1);
@@ -72,9 +53,11 @@ void flash() {
     digitalWrite(6, 1);
     digitalWrite(7, 1);
     digitalWrite(8, 1);
-
-    flagOn = true;
+    
+    flagOn = true;        //Переменная для хранения состояния реле. Реле было включено
   }
+  
+  // Если реле было включено, выключить
   if (flagOn) {
     if (millis() - counterPeriod > 1000) {
       digitalWrite(2, 0);
@@ -85,7 +68,7 @@ void flash() {
       digitalWrite(7, 0);
       digitalWrite(8, 0);
 
-      flagOn = false;
+      flagOn = false;// Изменение переменной состояния нагрузки пробуждения. Нагрузка была выключена
     }
   }
 }
@@ -96,6 +79,7 @@ void tempControl() {
   if (millis() - counterPeriodTemp > 1000) {
     counterPeriodTemp = millis();
     reading = analogRead(A0);        // получаем значение с аналогового входа A0
+    //Serial.println(reading);
     tempC = reading / 9.31;          // переводим в цельсии
     Serial.println(tempC);           // Отправка температуры на телефон
     //При достижении указанной температуры отключить реле
